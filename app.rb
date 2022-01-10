e18eafe15977e06afe9200d3bf32863f51973245
MichaelBodendorf <bodi@gmx.at>
2022-01-10 16:31:03 +0100

    add cat picture

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
-# Sinatra::Reloader
+Sinatra::Reloader
 
 get "/" do 
   "hello!"
@@ -11,3 +11,8 @@ get "/secret" do
   "hello Marcus and Michael!"
 end
   
+get '/cat' do 
+  "<div style='border: 3px dashed black'>
+    <img src='https://m.media-amazon.com/images/I/51KNy3fAsgL._AC_.jpg'>
+  </div>"
+end
